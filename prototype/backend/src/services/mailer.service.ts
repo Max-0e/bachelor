fdfe32dfe6c5620428fc304778fcc24a544cca10
max <oedinger13@gmail.com>
2023-03-24 12:32:11 +0100

    test pipeline

--- prototype/backend/src/services/mailer.service.ts
+++ prototype/backend/src/services/mailer.service.ts
@@ -14,6 +14,7 @@ class MailerService {
 				pass: SMTP_PASS,
 			},
 		});
+		console.log(SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS);
 		const message = {
 			from: '<maximilian.oedinger@hsrw.org>',
 			to: `<${config.recipientEmail}>`,
