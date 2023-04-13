cfb67afe6940f554e7d7689fba953f68ac8b9be1
max <oedinger13@gmail.com>
2023-04-13 12:37:02 +0200

    MOBA-148 link to api docs on home and removed http response logging in backend

--- prototype/frontend/src/config.ts
+++ prototype/frontend/src/config.ts
@@ -1,7 +1,9 @@
 export const API_URL: string =
 	import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
-export const DEV_MODE: boolean =
-	import.meta.env.VITE_DEV_MODE === 'true' || true;
+export const DEV_MODE: boolean = import.meta.env.DEV;
+export const SWAGGER_DOCS_URL: string = DEV_MODE
+	? 'http://localhost:3000/api-docs'
+	: 'https://116.203.140.167.nip.io/api-docs';
 
 export const emailRegex =
 	/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
